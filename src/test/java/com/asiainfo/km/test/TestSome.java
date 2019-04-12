10b850daa741f3359d118896fbf29d3cb0e8b74a
Timothy.ji <sevendark@yeah.net>
2019-04-12 15:11:25 +0800

    test and pom change

--- src/test/java/com/asiainfo/km/test/TestSome.java
+++ src/test/java/com/asiainfo/km/test/TestSome.java
@@ -1,21 +1,7 @@
 package com.asiainfo.km.test;
 
-import com.asiainfo.km.util.OsFileUtil;
 import org.junit.Test;
-import org.tmatesoft.svn.core.SVNDepth;
-import org.tmatesoft.svn.core.SVNException;
-import org.tmatesoft.svn.core.SVNURL;
-import org.tmatesoft.svn.core.auth.ISVNAuthenticationManager;
-import org.tmatesoft.svn.core.internal.io.dav.DAVRepositoryFactory;
-import org.tmatesoft.svn.core.internal.io.fs.FSRepositoryFactory;
-import org.tmatesoft.svn.core.internal.io.svn.SVNRepositoryFactoryImpl;
-import org.tmatesoft.svn.core.internal.wc.DefaultSVNOptions;
-import org.tmatesoft.svn.core.io.SVNRepository;
-import org.tmatesoft.svn.core.io.SVNRepositoryFactory;
-import org.tmatesoft.svn.core.wc.*;
 
-import java.io.File;
-import java.io.IOException;
 import java.sql.Timestamp;
 
 /**
@@ -23,41 +9,6 @@ import java.sql.Timestamp;
  */
 
 public class TestSome {
-    private String svnRoot = "svn://localhost/asiainfokm";
-    private String localRoot = "C:\\\\Users\\\\jiyuze\\\\Desktop\\\\km_repo";
-    private SVNRepository repository;
-    private SVNClientManager clientManager;
-    @Test
-    public void testSome() throws SVNException, IOException {
-        DAVRepositoryFactory.setup();
-        SVNRepositoryFactoryImpl.setup();
-        FSRepositoryFactory.setup();
-        repository = SVNRepositoryFactory.create(SVNURL.parseURIEncoded(svnRoot));
-        ISVNAuthenticationManager authManager = SVNWCUtil.createDefaultAuthenticationManager("jyz", "jyz");
-        repository.setAuthenticationManager(authManager);
-        DefaultSVNOptions options = SVNWCUtil.createDefaultOptions(true);
-        clientManager = SVNClientManager.newInstance(options,authManager);
-
-        SVNWCClient svnwcClient = clientManager.getWCClient();
-        File a = OsFileUtil.newFileByOs(localRoot,"DOC_2017120505000303.java");
-        System.out.println(a.getPath());
-        System.out.println(a.getName());
-        System.out.println(a.getAbsolutePath());
-        System.out.println(a.getCanonicalPath());
- /*     File b = OsFileUtil.newFileByOs(localRoot,"2222");
-        Boolean key = b.renameTo(a);
-        if(key){
-            clientManager.getWCClient().doAdd(new File[] { a }, true,
-                    true, false, SVNDepth.INFINITY, false, false, true);
-        }*/
-        svnwcClient.doDelete(a,true,true,false);
-        clientManager.getCommitClient().doCommit(new File[]{a},false,"111",false,false);
-
-        SVNUpdateClient updateClient = clientManager.getUpdateClient();
-        File path = new File(localRoot);
-        updateClient.doUpdate(path, SVNRevision.HEAD,SVNDepth.INFINITY,false,true);
-
-    }
 
     @Test
     public void testSome2() throws Exception {
